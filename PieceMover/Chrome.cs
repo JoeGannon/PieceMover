1e17a126f1ef7ae7dd5912d97609a7a2ede5cbe5
Joe Gannon <joegannon90@yahoo.com>
2018-09-30 15:03:57 -0400

    proof of concept

---
+++ PieceMover/Chrome.cs
@@ -0,0 +1,35 @@
+﻿namespace PieceMover
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+    //https://stackoverflow.com/a/30156990/2612547
+    public class Chrome
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private const uint _keyDown = 0x100;
+        private const uint _keyUp = 0x101;
+
+        private readonly Process chromeProcess;
+
+        public Chrome()
+        {
+            chromeProcess = Process.GetProcessesByName("chrome").First();
+        }
+
+        public void SendKey(char key)
+        {
+            if (chromeProcess.MainWindowHandle != IntPtr.Zero)
+            {
+                SendMessage(chromeProcess.MainWindowHandle, _keyDown, (IntPtr)key, IntPtr.Zero);
+                SendMessage(chromeProcess.MainWindowHandle, _keyUp, (IntPtr)key, IntPtr.Zero);
+            }
+        }
+    }
+
+}
---
+++ PieceMover/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PieceMover
+{
+    using System.Threading;
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var chrome = new Chrome();
+            Thread.Sleep(3000);
+
+            new External().SendInputWithAPI();
+
+            chrome.SendKey(Keys.Enter);
+        }
+    }
+}
